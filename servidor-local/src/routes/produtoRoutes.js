96d565a7cc0acd7b93017e0a0eb1eaf6f71c8788
ismar222-code <ismaralfama222@gmail.com>
2026-09-04 11:17:40 -0100

    feat: add LampContainer component for animated hero section background effects

--- servidor-local/src/routes/produtoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const produtoController = require('../controllers/produtoController');
-
-// Rota para listar todos os produtos
-router.get('/', produtoController.listarProdutos);
-
-module.exports = router;
